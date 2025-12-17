e68013a2b91434b28d4bd1a0e1486f402f86726d
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2025-12-17 15:42:06 +0200

    Update AdminPageController.java

--- PL2_Project/src/main/java/com/example/pl2_project/AdminPageController.java
+++ PL2_Project/src/main/java/com/example/pl2_project/AdminPageController.java
@@ -1,6 +1,33 @@
 package com.example.pl2_project;
 
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class AdminPageController {
 
+    public TextField add_ID_TF;
+    public TextField add_Username_TF;
+    public PasswordField add_Password_TF;
+    public ComboBox add_Role_TF;
+    public Button addUserButton;
+    public TextField update_ID_TF;
+    public TextField update_Username_TF;
+    public TextField update_Password_TF;
+    public ComboBox update_Role_TF;
+    public Button updateUserButton;
+    public TextField delete_ID_TF;
+    public Button deleteUserButton;
+
+    public void clickOnAddButton(ActionEvent actionEvent) {
+    }
+
+    public void clickOnUpdateButton(ActionEvent actionEvent) {
+    }
+
+    public void clickOnDeleteButton(ActionEvent actionEvent) {
+    }
 }
